95d376e53da54f1ffdaf1ed289da8abce5d9d677
alexx sub <alexei.s@list.ru>
2022-04-28 09:58:50 +0300

    fixed

--- cpu_top_linux.sh
+++ cpu_top_linux.sh
@@ -4,5 +4,5 @@ timestamp=$(date +%s%N)
 hostname=$(hostname | tr a-z A-Z)
 scriptname=${0##*/}
 
-cpu_busy=$(top -bn 2 -d 0.01 | grep '^%Cpu' | tail -n 1 | awk '{print $2+$4+$6}')
-echo "cpu_percent,host=$hostname,script=$scriptname cpu_busy=$(printf "%.2f" $cpu_busy) $timestamp"
+cpu_busy=$(top -bn 2 -d 0.01 | grep '^%Cpu' | tail -n 1 | awk '{print 100-$8}')
+echo "cpu_percent,host=$hostname,script=$scriptname cpu_busy=$cpu_busy $timestamp"
